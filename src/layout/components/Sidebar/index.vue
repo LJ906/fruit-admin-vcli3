1996fff161f00b3f258941d714848c70a07b68d3
LJ906 <ni_hao906@163.com>
2019-11-12 19:29:40 +0800

    添加菜单及折叠按钮

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,59 +1,125 @@
 <template>
   <div :class="{'has-logo':showLogo}">
-    <logo v-if="showLogo" :collapse="isCollapse" />
-
-    <el-menu
-      default-active="1-4-1"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
-      :collapse="isCollapse"
-    >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
+    <!-- <logo v-if="showLogo" :collapse="isCollapse" /> -->
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item
+          v-for="route in menuList"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+// import Logo from './Logo'
+import SidebarItem from "./SidebarItem";
+import variables from "@/styles/variables.scss";
+
 export default {
-  name: "Sidebar",
+  components: { SidebarItem },
   data() {
     return {
-      showLogo: false,
-      isCollapse: false
+      menuList: [
+        {
+          path: "/charts",
+          // component: Layout,
+          redirect: "noRedirect",
+          name: "Charts",
+          meta: {
+            title: "Charts",
+            icon: "chart"
+          },
+          children: [
+            {
+              path: "keyboard",
+              // component: () => import('@/views/charts/keyboard'),
+              name: "KeyboardChart",
+              meta: { title: "Keyboard Chart", noCache: true }
+            },
+            {
+              path: "line",
+              // component: () => import('@/views/charts/line'),
+              name: "LineChart",
+              meta: { title: "Line Chart", noCache: true }
+            },
+            {
+              path: "mix-chart",
+              // component: () => import('@/views/charts/mix-chart'),
+              name: "MixChart",
+              meta: { title: "Mix Chart", noCache: true }
+            }
+          ]
+        },
+        {
+          path: "/charts2",
+          // component: Layout,
+          redirect: "noRedirect",
+          name: "Charts",
+          meta: {
+            title: "Charts2",
+            icon: "chart"
+          },
+          children: [
+            {
+              path: "keyboard",
+              // component: () => import('@/views/charts/keyboard'),
+              name: "KeyboardChart",
+              meta: { title: "Keyboard Chart", noCache: true }
+            },
+            {
+              path: "line",
+              // component: () => import('@/views/charts/line'),
+              name: "LineChart",
+              meta: { title: "Line Chart", noCache: true }
+            },
+            {
+              path: "mix-chart",
+              // component: () => import('@/views/charts/mix-chart'),
+              name: "MixChart",
+              meta: { title: "Mix Chart", noCache: true }
+            }
+          ]
+        }
+      ]
     };
+  },
+  computed: {
+    ...mapGetters([
+      // "permission_routes",
+      "sidebar" // 记录在vuex的侧边栏的状态
+    ]),
+    activeMenu() {
+      const route = this.$route;
+      const { meta, path } = route;
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu;
+      }
+      return path;
+    },
+    showLogo() {
+      return this.$store.state.settings.sidebarLogo;
+    },
+    variables() {
+      return variables;
+    },
+    isCollapse() {
+      return !this.sidebar.opened;
+    }
   }
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
